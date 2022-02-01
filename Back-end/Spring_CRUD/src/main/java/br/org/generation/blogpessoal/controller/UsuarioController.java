289b8606c722062fa928249cabda119aaa9796ff
Vitória Santos Oliveira <contato.santos.vitoria12@gmail.com>
2022-02-01 15:09:07 -0300

    adicionando test

--- Back-end/Spring_CRUD/src/main/java/br/org/generation/blogpessoal/controller/UsuarioController.java
+++ Back-end/Spring_CRUD/src/main/java/br/org/generation/blogpessoal/controller/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,8 +37,14 @@ public class UsuarioController {
 	public ResponseEntity <List<Usuario>> getAll(){
 		
 		return ResponseEntity.ok(usuarioRepository.findAll());
-		
 	}
+	
+	@GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id){
+        return usuarioRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.notFound().build());
+    }
 	
 	@PostMapping("/logar")
 	public ResponseEntity<UsuarioLogin> login(@RequestBody Optional<UsuarioLogin> user) {
